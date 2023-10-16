7e80395a5eb2188f86f732ea1af39e8f388434fe
manulopez14 <mlopez14@ieszaidinvergeles.org>
2023-10-16 22:58:02 +0200

    initial commit

---
+++ tsystemstore/src/main/java/com/mrojasdev/tsystemstore/TsystemstoreApplication.java
@@ -0,0 +1,13 @@
+package com.mrojasdev.tsystemstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsystemstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsystemstoreApplication.class, args);
+	}
+
+}
---
+++ tsystemstore/src/test/java/com/mrojasdev/tsystemstore/TsystemstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrojasdev.tsystemstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsystemstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
